e8225ba68964d9cd1594b8c98adae7af650f2705
Dallin Harkness <dalhark99@gmail.com>
2021-12-23 10:09:52 -0700

    finished

---
+++ steptwo.js
@@ -0,0 +1,29 @@
+
+// add to zero
+let array = [28, 43, -12, 30, 4, 0, 12]
+
+let value = "False"
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (i !== j) {
+      if (array[i] + array[j] === 0) {
+        value = "True"
+      } 
+    }
+  }
+}
+
+console.log(value) //O(n^2)
+
+//Unique Characters
+function hasUniqueChars(word) {
+    let uniqueChars = new Set([])
+    for (let i = 0; i < word.length; i++) {
+      uniqueChars.add(word[i])
+    }
+    return uniqueChars.size === word.length
+  }
+  
+  hasUniqueChars('mooon') //O(1)
+
+  
